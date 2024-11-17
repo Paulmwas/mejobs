dd58f2a0595dd01a70908468542bbe539dbec709
Paul <mwapaul265@gmail.com>
2024-11-17 10:08:45 +0300

    first commit

---
+++ authpages/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+# Create your views here.
+def signup_page(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists')
+        else:
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+            messages.success(request, 'Account created successfully')
+            return redirect('login')
+    return render(request, 'authpages/sign_up.html')
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid credentials')
+    return render(request, 'authpages/login.html')
+
+
+def logout_page(request):
+    logout(request)
+    messages.success(request, 'Logout successful')
+    return redirect('login')
